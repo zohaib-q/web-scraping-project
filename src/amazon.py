6aed42f98b36f7a113ed08cd4c9da0ed56768740
zohaib-q <zquraishi@cox.net>
2024-08-05 21:19:19 -0400

    test changes

--- src/amazon.py
+++ src/amazon.py
@@ -3,6 +3,7 @@ import requests
 import webbrowser
 import pandas as pd
 
+#need headers in order to webscrape these websites
 HEADERS = {
     'User-Agent': ('Mozilla/5.0 (X11; Linux x86_64)'
                     'AppleWebKit/537.36 (KHTML, like Gecko)'
